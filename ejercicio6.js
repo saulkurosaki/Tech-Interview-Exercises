1b05286d0e04c74cbf59fdadf056bd38504ebbbe
Saul Paredes <saulkurosaki13@gmail.com>
2023-08-08 20:55:53 -0600

    ejercicio6.js

---
+++ ejercicio6.js
@@ -0,0 +1,41 @@
+
+//Ejercicio6: Dibujar un cuadrado hueco con asteriscos
+//            ****
+//            *  *
+//            *  *
+//            ****
+
+const lado = (lados) => {
+    let lado = '';
+
+    for(let i = 0; i < lados; i++){
+        lado += '*';
+    };
+
+    return lado
+};
+
+const cuadrado = (num) => {
+    let dibujo = lado(num) + '\n';
+
+    let contenido = '';
+    for(let i = 2; i < num; i++){
+
+        contenido = '*';
+
+        for(let x = 2; x < num; x++){
+            contenido += ' ';
+        };
+
+        contenido += '*'
+
+        dibujo += contenido + '\n';
+    };
+
+    dibujo += lado(num);
+
+    return dibujo
+};
+
+
+console.log(cuadrado(8));
